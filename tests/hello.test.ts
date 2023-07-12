b891727f0a274a65f1cb126f31948416920e240b
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-07-12 19:14:04 +0700

    first commit

---
+++ tests/hello.test.ts
@@ -0,0 +1,6 @@
+describe('Hello', function(){
+      it("should say hello there", function(){
+            const name = "Hello Syauqi";
+            expect(name).toBe("Hello Syauqi");
+      });
+});
